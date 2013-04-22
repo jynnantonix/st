ce92ffc605636e54197b6a5700e5d2dcb9aa51af
Chirantan Ekbote <chirantan.ekbote@gmail.com>
2013-04-22 17:05:29 -0400

    Use terminus font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=false:autohint=false";
+static char font[] = "-*-terminus-medium-r-*-*-10-*-*-*-*-*-*-*";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 
